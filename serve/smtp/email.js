13e922dbdead4962d865711d52da49e41b163f1e
神月恭平 <tuzwyx@foxmail.com>
2024-08-26 08:23:48 +0000

    update serve/smtp/email.js.
    Signed-off-by: 神月恭平 <tuzwyx@foxmail.com>

--- serve/smtp/email.js
+++ serve/smtp/email.js
@@ -15,8 +15,8 @@ async function sendEmail(options) {
         port: 465,
         secure: true,
         auth: {
-            user: 'tuzwyx@foxmail.com',
-            pass: 'qpfnnsvadoukbcda'
+            user: '你的QQ邮箱',
+            pass: '你的QQ邮箱SMTP授权码'
         }
     });
 
@@ -43,35 +43,5 @@ async function sendEmail(options) {
         console.error('Error sending email:', error);
     }
 }
-
-
-// async function sendEmail(email) {
-//     let transporter = nodemailer.createTransport({
-//         service: 'qq', // 注意：'qq' 不是直接支持的，但你可以使用通用的 SMTP 设置
-//         // 或者使用以下的 SMTP 设置（更通用的方式）
-//         host: 'smtp.qq.com',
-//         port: 465, // SMTP 端口，对于 SSL 是 465，对于 STARTTLS 是 587
-//         secure: true, // 启用 SSL
-//         auth: {
-//             user: 'tuzwyx@foxmail.com', // 你的QQ邮箱地址
-//             pass: 'qpfnnsvadoukbcda' // 你的QQ邮箱SMTP授权码
-//         }
-//     });
-
-//     let mailOptions = {
-//         from: '"团子网"tuzwyx@foxmail.com', // 发送者地址
-//         to: email, // 接收者邮箱
-//         subject: "测试邮件", // 主题行
-//         // text: "你看到这封邮件，代表邮件服务配置正确啦！", // 纯文本正文
-//         //<p>你看到这封邮件，代表邮件服务配置正确啦！</p><p>恭喜你可以使用邮件服务啦！</p>
-//         // HTML正文
-//         html: ""
-//     };
-
-//     let info = await transporter.sendMail(mailOptions);
-
-//     console.log('Message sent: %s', info.messageId);
-// }
-//sendEmail().catch(console.error);
 
 module.exports = sendEmail;
